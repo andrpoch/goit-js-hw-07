569155494231084d5830794a7fbe92008cdd9c76
andrpoch <andrpoch25@gmail.com>
2022-02-05 15:09:16 +0200

    Finished first task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,28 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+   
+const markup = galleryItems.map(({ description, original, preview }) => `<div class="gallery__item">
+   <a class="gallery__link" href='${original}'>
+   <img class="gallery__image" src='${preview}' data-source='${original}'
+   alt='${description}'/></a></div>`)
+   .join('');
+gallery.insertAdjacentHTML("afterbegin", markup);
+
+gallery.addEventListener(`click`, onClick);
+
+function onClick(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  galleryItems.map((item) => {
+    if (item.original === event.target.dataset.source) {
+      const instance = basicLightbox.create(` 
+        <img src=${item.original} width="800" height="600">
+    `);
+    instance.show();
+    }
+   }
+  )   
+ }
